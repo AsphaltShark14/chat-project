459af739d5771cac64e9fa7ea1e2bc052c49af5f
Daniel Bogucki <daniel.bogucki14@gmail.com>
2021-12-14 19:42:18 +0100

    Added JS functionality

---
+++ scripts/ui.js
@@ -0,0 +1,18 @@
+//render chat templtes to the DOM
+// clear the list of chats (when room changes)
+
+class ChatUI {
+  constructor(list){
+    this.list = list;
+  }
+  render(data){
+    const html = `
+      <li class="list-group-item">
+        <span class="username">${data.username}</span>
+        <span class="message">${data.message}</span>
+        <div class="time">${data.created_at.toDate()}</span>
+      </li>
+    `;
+    this.list.innerHTML += html;
+  }
+}
