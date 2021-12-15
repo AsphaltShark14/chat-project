baf587a3e6a389cf9dbc6e7c28078733dd1221e7
Daniel Bogucki <daniel.bogucki14@gmail.com>
2021-12-15 19:29:17 +0100

    Added UI update methods, changing username and chatroom

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,48 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMessage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+
+    newNameForm.reset();
+
+    updateMessage.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMessage.innerText = '', 3000)
+    
+});
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';
+
+// update the chat room
+rooms.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(data => chatUI.render(data));
+    }
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'shaun');
+const chatroom = new Chatroom('general', username);
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
